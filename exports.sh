c7c4b4cb72a81f41b6da6e7812d9ef95b86d13e7
Duong Tran <duong.tran@pyramid-consulting.com>
2016-12-29 14:46:41 +0700

    - Update config path

--- exports.sh
+++ exports.sh
@@ -2,12 +2,14 @@
 
 set -e
 
-API_KEY=$(cat config.json | jq -r '. | .api_key')
-PROJECT_ID=$(cat config.json | jq -r '. | .project_id')
-LANGUAGE_CODES=$(cat config.json | jq -r '. | .languages')
-FILE_NAME=$(cat config.json | jq -r '. | .file_name')
-FILE_FORMAT=$(cat config.json | jq -r '. | .file_format')
-FILE_PATH=$(cat config.json | jq -r '. | .path')
+export CONFIG_FILE_PATH=$1
+
+API_KEY=$(cat $CONFIG_FILE_PATH | jq -r '. | .api_key')
+PROJECT_ID=$(cat $CONFIG_FILE_PATH | jq -r '. | .project_id')
+LANGUAGE_CODES=$(cat $CONFIG_FILE_PATH | jq -r '. | .languages')
+FILE_NAME=$(cat $CONFIG_FILE_PATH | jq -r '. | .file_name')
+FILE_FORMAT=$(cat $CONFIG_FILE_PATH | jq -r '. | .file_format')
+FILE_PATH=$(cat $CONFIG_FILE_PATH | jq -r '. | .path')
 
 set -f                      # avoid globbing (expansion of *).
 array=(${LANGUAGE_CODES//,/ })
