29f27277fb485bab7b341e3f8329a4fa45f3aaa3
Duong Tran <duong.tran@pyramid-consulting.com>
2017-01-09 14:43:39 +0700

    - Image generator

---
+++ image-generator.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -e
+
+export IMAGE_PATH=$1
+OUTPUT_PATH=$2
+
+config_path="`pwd`/$IMAGE_PATH"
+output_path="`pwd`/$OUTPUT_PATH/images.js"
+
+find="-"
+replace="_"
+
+echo "const Images = {" > $output_path
+for file in "$config_path/"*
+do
+    filename=$(basename "$file")
+    fileNameWithoutFormat="${filename%.*}"
+
+    fileNameWithoutFormat="${fileNameWithoutFormat/\-/_}"
+
+    echo -e "\t$fileNameWithoutFormat: require('./images/$filename')," >> $output_path
+done
+
+echo "};" >> $output_path
+echo "export default Images;" >> $output_path
